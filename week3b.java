b2d9dbcfdc2bfdd5a92a151cbfc939a2a8bed5e1
badallll <phuyall.badal333@gmail.com>
2021-12-03 20:15:16 +0545

    final java

---
+++ week3b.java
@@ -0,0 +1,68 @@
+import java.util.*;
+    public class week3b {
+
+            static boolean areBalanceweek4b (String expr)
+            {
+                Deque<Character> stack
+                        = new ArrayDeque<Character>();
+
+                for (int i = 0; i < expr.length(); i++)
+                {
+                    char x = expr.charAt(i);
+
+                    if (x == '(' || x == '[' || x == '{')
+                    {
+                        stack.push(x);
+                        continue;
+                    }
+
+                    if (stack.isEmpty())
+                        return false;
+                    char check;
+                    switch (x) {
+                        case ')':
+                            check = stack.pop();
+                            if (check == '{' || check == '[')
+                                return false;
+                            break;
+
+                        case '}':
+                            check = stack.pop();
+                            if (check == '(' || check == '[')
+                                return false;
+                            break;
+
+                        case ']':
+                            check = stack.pop();
+                            if (check == '(' || check == '{')
+                                return false;
+                            break;
+                    }
+                }
+
+                return (stack.isEmpty());
+            }
+
+            public static void main(String[] args)
+            {
+                String expr = " [{()[{(";
+
+                if (areBalanceweek4b(expr))
+                    System.out.println("[([(” ");
+                else
+                    System.out.println(": [(, [{, {(  ");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
